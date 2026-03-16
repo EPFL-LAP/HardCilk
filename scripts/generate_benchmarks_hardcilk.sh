edf030caa5f57dbfdeab73be93042faccb7d0465
Mahfouz-z <mohamed.shahawy@epfl.ch>
2026-03-16 01:49:25 +0100

    Correcting the HBM port count of graphRandomWalk

--- scripts/generate_benchmarks_hardcilk.sh
+++ scripts/generate_benchmarks_hardcilk.sh
@@ -1,4 +1,4 @@
 cd ../architecture-generator
-sbt "runMain HardCilk.HardCilkEmitter taskDescriptors/mfpga/graphRandomWalk.json -o ../HardCilk-output/ -g -c -r 26 -p"
+sbt "runMain HardCilk.HardCilkEmitter taskDescriptors/mfpga/graphRandomWalk.json -o ../HardCilk-output/ -g -c -r 22 -p"
 sbt "runMain HardCilk.HardCilkEmitter taskDescriptors/mfpga/pageRank.json -o ../HardCilk-output/ -g -c -r 22 -p"
 sbt "runMain HardCilk.HardCilkEmitter taskDescriptors/mfpga/triangleCount.json -o ../HardCilk-output/ -g -c -r 23 -p"
